5d94a4a90fb542a9a6019c0c7fb4eb34cdfe2164
dm1trous <lopic9999lokoko@gmail.com>
2025-06-15 17:54:04 +0500

    last

--- SiteClothes/users/models.py
+++ SiteClothes/users/models.py
@@ -7,6 +7,10 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Пользователь")
     image = models.ImageField(default='default.jpg', upload_to='profile_pics', verbose_name="Аватарка")
 
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
     def __str__(self):
         return f'Профиль пользователя {self.user.username}'
 
